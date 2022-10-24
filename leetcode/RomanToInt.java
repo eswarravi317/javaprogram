7bc7a4a90f865beb0bff4e6f8777204e38b9d764
Eswar <109469319+eswarravi317@users.noreply.github.com>
2022-10-24 18:25:22 +0530

    Update RomanToInt.java

--- leetcode/RomanToInt.java
+++ leetcode/RomanToInt.java
@@ -7,30 +7,30 @@ public class RomanToInt {
 		int result = 0, num = 0;
 		for(int i=roman.length()-1; i>=0; i--) {
 			switch(roman.charAt(i)) {
-			case 'I':
-                num = 1;
-                break;
-            case 'V':
-                num = 5;
-                break;
-            case 'X':
-                num = 10;
-                break;
-            case 'L':
-                num = 50;
-                break;
-            case 'C':
-                num = 100;
-                break;
-            case 'D':
-                num = 500;
-                break;
-            case 'M':
-                num = 1000;
-                break;
+			    case 'I':
+				num = 1;
+				break;
+			    case 'V':
+				num = 5;
+				break;
+			    case 'X':
+				num = 10;
+				break;
+			    case 'L':
+				num = 50;
+				break;
+			    case 'C':
+				num = 100;
+				break;
+			    case 'D':
+				num = 500;
+				break;
+			    case 'M':
+				num = 1000;
+				break;
 			}
 			if (4 * num < result) result -= num;
-            else result += num;
+			else result += num;
 		}
 		return result;
 	}
@@ -40,6 +40,7 @@ public class RomanToInt {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Input Roman: ");
 		String roman = sc.nextLine();
+		sc.close();
 		System.out.println("Output Int: "+ obj.romanToInt(roman));
 	}
 	
